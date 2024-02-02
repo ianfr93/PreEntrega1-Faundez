5a3ba52f71ce517113d37edf2d01c5cf419c1bce
ianfr93 <albitox10@gmail.com>
2024-02-01 21:43:58 -0300

    cambios

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,11 +1,29 @@
-// Menu de inicio de sesión
-let usuarios = [
-  { usuario: "usuario1", contraseña: "contraseña1" },
-  { usuario: "usuario2", contraseña: "contraseña2" },
-  { usuario: "usuario3", contraseña: "contraseña3" },
-  { usuario: "usuario4", contraseña: "contraseña4" },
-  { usuario: "usuario5", contraseña: "contraseña5" },
-  { usuario: "usuario6", contraseña: "contraseña6" }
+// menu de inicio de sesion
+let usuarios = [{
+    usuario: "usuario1",
+    contraseña: "contraseña1"
+  },
+
+  {
+    usuario: "usuario2",
+    contraseña: "contraseña2"
+  },
+  {
+    usuario: "usuario3",
+    contraseña: "contraseña3"
+  },
+  {
+    usuario: "usuario4",
+    contraseña: "contraseña4"
+  },
+  {
+    usuario: "usuario5",
+    contraseña: "contraseña5"
+  },
+  {
+    usuario: "usuario6",
+    contraseña: "contraseña6"
+  }
 ];
 
 function autenticarUsuario() {
@@ -13,21 +31,25 @@ function autenticarUsuario() {
   let contraseña = prompt("Ingrese su contraseña");
   let usuarioValido = usuarios.find(user => user.usuario === nombreUsuario && user.contraseña === contraseña);
 
-  // si los datos están mal ingresados
+  // si los datos estan mal ingresados
   if (!usuarioValido) {
     alert("Nombre de usuario o contraseña incorrectos. Intente nuevamente.");
     autenticarUsuario();
   }
 }
-
 // Luego, llama a la función para autenticar al usuario
 autenticarUsuario();
 
+let Selecione_Caja;
 let producto;
 let precio;
 let total_compra = 0;
 let salir;
 let error;
+
+function pedir_datos_cliente() {
+  Selecione_Caja = prompt('Indique caja');
+}
 
 function cargar_productos() {
   do {
@@ -38,7 +60,7 @@ function cargar_productos() {
       "3-lavalozas quix 1lt" + "\n" +
       "4-leche soprole chocolate 1lt" + "\n" +
       "5-galletas oreo chocolate" + "\n" +
-      "6-arroz miraflores granel"));
+      "6-arroz miraflores granel"))
 
     switch (producto) {
       case 1:
@@ -65,19 +87,28 @@ function cargar_productos() {
         precio = 1600;
         producto = "arroz miraflores granel";
         break;
+
       default:
         alert('Opción incorrecta. Digite nuevamente');
         error = 1;
     }
   } while (error == 1);
+
+  return // producto, precio
 }
 
-// Solicito productos
-cargar_productos();
+// Comienzo con el programa
+alert("Bienvenido al proceso de compra");
+
+// Solicito datos al cliente
+pedir_datos_cliente();
 
 do {
+  // Solicito cliente al usuarios
+  cargar_productos();
+
   // Solicito la cantidad del producto
-  let cantidad = parseInt(prompt("¿Qué cantidad del " + producto + " desea llevar?"));
+  cantidad = parseInt(prompt("¿Qué cantidad del " + producto + " desea llevar?"));
 
   console.log(producto);
   console.log(precio);
@@ -88,19 +119,14 @@ do {
 
   salir = prompt('Desea agregar otro producto en el carrito de compras? SI/NO');
 
-  if (salir.toUpperCase() === 'SI') {
-    // Solicito productos nuevamente si el usuario quiere agregar más
-    cargar_productos();
-  }
-
-} while (salir.toUpperCase() === 'SI');
+} while (salir.toUpperCase() != 'NO');
 
 // Aplicar IVA
 const iva = total_compra * 0.19;
 const total_con_iva = total_compra + iva;
 
-console.log(nombreUsuario); // Esto dará un error ya que nombreUsuario no está definido en este contexto
+console.log(nombreUsuario);
 console.log(total_compra);
 
 // Muestro al cliente el total de su compra con IVA
-alert(`El total de tu compra (con IVA) fue de $${total_con_iva.toFixed(2)}`);
+alert(`${nombreUsuario.toUpperCase()}, el total de tu compra (con IVA) fue de $${total_con_iva.toFixed(2)}`);
